185b600859bd6b43027c2e67f146160543aaa51d
Aaron Binford <abinford@cleversafe.com>
2018-06-13 16:36:57 -0500

    handle IBM Cloud Object Storage 429 responses

--- lib/ex_aws/request.ex
+++ lib/ex_aws/request.ex
@@ -38,16 +38,16 @@ defmodule ExAws.Request do
       case config[:http_client].request(method, safe_url, req_body, full_headers, Map.get(config, :http_opts, [])) do
         {:ok, %{status_code: status} = resp} when status in 200..299 or status == 304 ->
           {:ok, resp}
+        {:ok, %{status_code: status} = resp} when status == 429 or status >= 500 ->
+          body = Map.get(resp, :body)
+          reason = {:http_error, status, body}
+          request_and_retry(method, url, service, config, headers, req_body, attempt_again?(attempt, reason, config))
         {:ok, %{status_code: status} = resp} when status in 400..499 ->
           case client_error(resp, config[:json_codec]) do
             {:retry, reason} ->
               request_and_retry(method, url, service, config, headers, req_body, attempt_again?(attempt, reason, config))
             {:error, reason} -> {:error, reason}
           end
-        {:ok, %{status_code: status} = resp} when status >= 500 ->
-          body = Map.get(resp, :body)
-          reason = {:http_error, status, body}
-          request_and_retry(method, url, service, config, headers, req_body, attempt_again?(attempt, reason, config))
         {:error, %{reason: reason}} ->
           Logger.warn("ExAws: HTTP ERROR: #{inspect reason}")
           request_and_retry(method, url, service, config, headers, req_body, attempt_again?(attempt, reason, config))
